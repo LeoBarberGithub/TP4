f0f1a82bc8e702a0ea93037e1aa26e9739c9b03f
Prof Info <66843772+Cordier-Info@users.noreply.github.com>
2020-07-15 12:46:41 +0200

    Update exo_1.py

--- exo_1.py
+++ exo_1.py
@@ -1,1 +1,2 @@
-
+# message à traduire
+énoncé = "Épever ha cebtenzzr dhv genafsbezr ha grkgr qr znavèer à pr dhr yn cerzvèer rg yn qreavèer yrgger qr pundhr zbg erfgr vapunatér nybef dhr yrf yrggerf qr zvyvrh fbag zéynatérf nh unfneq. Ln cbapghngvba qbvg erfgre ra cynpr. Oa cbheen hgvyvfre yn sbapgvba enaqbz.fuhssyr() qh cnpxntr enaqbz dhv zéynatr yrf qvsséeragf éyézragf dh'ba yhv qbaar ra nethzrag."
