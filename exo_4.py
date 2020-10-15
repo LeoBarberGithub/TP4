68f20c9d13945daa58ccc1a08eba4c494dc744cc
LeoBarberGithub <70711310+LeoBarberGithub@users.noreply.github.com>
2020-10-15 11:27:21 +0200

    Update exo_4.py

--- exo_4.py
+++ exo_4.py
@@ -1,1 +1,15 @@
-
+Code=input(str("Code:"))
+inverse=Code[::-1]
+somme=0
+for i in range(1,len(inverse),2):
+  int (i)
+  i*=2
+  if i>9:
+   i-=9
+  somme+=i
+for i in range (0,len(inverse),2):
+ somme +=int(i)
+if somme%10 == 0:
+ print('Valide')
+else:
+ print('Invalide')
